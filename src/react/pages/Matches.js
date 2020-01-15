7aeeade64f11efcb771f586f21b49cb1e0ef87ec
Darveloper <darveloper@gmail.com>
2020-01-14 19:17:07 -0500

    matches

--- src/react/pages/Matches.js
+++ src/react/pages/Matches.js
@@ -2,12 +2,15 @@ import React from "react";
 import FriendsAction from "../components/FriendsAction";
 import "./Matches.css";
 import 'antd/dist/antd.css';
+// import Sidemenu from "../components/SideMenu"
 
 class Matches extends React.Component {
   render() {
     return (
       <div>
+        {/* <Sidemenu /> */}
         <h1>Matches</h1>
+      
         <div className="container" style={{ height: "100vh" }}>
           <div className="UserCard">
             <FriendsAction className="MatchCard" />
