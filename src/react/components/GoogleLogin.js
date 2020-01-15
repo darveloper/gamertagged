a7f1a8f03e5aabfe558a39b88cc5d0bb2d9154cd
Darveloper <darveloper@gmail.com>
2020-01-15 12:21:21 -0500

    added google-login style updates from Dan

--- src/react/components/GoogleLogin.js
+++ src/react/components/GoogleLogin.js
@@ -1,29 +1,24 @@
-import React, { Component } from 'react';
-// import './GoogleLogin.css';
-import GoogleLogin from 'react-google-login';
+import React, { Component } from "react";
+import "./GoogleLogin.css";
+import GoogleLogin from "react-google-login";
 
-class GoogleLoginFeature extends Component {
+class GoogleLoginForm extends Component {
+  render() {
+    const responseGoogle = response => {
+      console.log(response);
+    };
 
-    render() {
-  
-      const responseGoogle = (response) => {
-        console.log(response);
-      }
-
-      return (
-        <div className="App">
-          <h1>LOGIN WITH GOOGLE</h1>
-  
+    return (
+      <div className="App" style={{ margin: "auto" }}>
         <GoogleLogin
           clientId="869187170200-3dtdrjohk5k63ogj0roa8l9k4gpmjr88.apps.googleusercontent.com"
           buttonText="LOGIN WITH GOOGLE"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
         />
-  
-        </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  export default GoogleLoginFeature;
+}
+
+export default GoogleLoginForm;
