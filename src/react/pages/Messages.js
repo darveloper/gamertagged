d5715079545b275cc96dba9f305e58c34eeb6331
Darveloper <darveloper@gmail.com>
2020-01-15 11:10:56 -0500

    messages

---
+++ src/react/pages/Messages.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Messaging from "../components/Messaging";
+
+class Messages extends React.Component {
+    render() {
+      return (
+        <div>
+          <Messaging />
+        </div>
+      );
+    }
+  }
+  
+  export default Messages;
--- src/react/pages/index.jsx
+++ src/react/pages/index.jsx
@@ -4,13 +4,14 @@ import NotFound from "./NotFound";
 import Matches from "./Matches";
 import Register from "./Register";
 import Questionnaire from "./Questionnaire"
-
+import Messages from "./Messages"
 
 export default {
   Home: { path: "/", component: Home },
   Profile: { path: "/profile", component: Profile },
   Matches: { path: "/matches", component: Matches },
   Register: { path: "/register", component: Register },
+  Messages: { path: "/messages", component: Messages },
   Questionnaire: { path: "/questionnaire", component: Questionnaire },
   NotFound: { path: "*", component: NotFound }
 };
