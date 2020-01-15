bf2de66bedea3232de251d5cf8ef10237d45532d
Janelle Kuhns <j3ckuhns@gmail.com>
2020-01-15 17:03:42 +0000

    Update Home.js

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
         </div>
         <div className="login_container" style={{ marginTop: "5%" }}>
           <LoginForm />
-          <GoogleLogin />
+          <GoogleLoginForm />
         </div>
       </>
     );
