fdd99c0dbf1887d453e91bf3c471f2e052398264
Dan Birkbeck <DBirkbeck217@gmail.com>
2020-01-15 12:11:34 -0500

    updated google login & background-color

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import LoginForm from "../components/LoginForm";
 import "./Home.css";
-import { go } from "connected-react-router";
 import GoogleLogin from "../components/GoogleLogin";
 
 class Home extends Component {
