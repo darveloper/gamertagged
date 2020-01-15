92f03cd0a365d5bd4b1045c430dcbb52786ccd54
Darveloper <darveloper@gmail.com>
2020-01-15 13:16:23 -0500

    added go back button to messages page

--- src/react/pages/Messages.js
+++ src/react/pages/Messages.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Messaging from "../components/Messaging";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
 
 class Messages extends React.Component {
-    render() {
-      return (
-        <div>
-          <Messaging />
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <Messaging />
+        <Link to="/matches">
+          <Button type="primary">Go Back</Button>
+        </Link>
+      </div>
+    );
   }
-  
-  export default Messages;
+}
+
+export default Messages;
